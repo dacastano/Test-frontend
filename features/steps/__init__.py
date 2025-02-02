fdadf3c3d7a0e1233631986bec4b910e3a025a9e
Diego <dgcastanoa@gmail.com>
2025-02-01 19:53:36 -0500

    Small refactor on init file to properly scan modules, add gitignore, update some dependencies

--- features/steps/__init__.py
+++ features/steps/__init__.py
@@ -1,9 +1,17 @@
 import os
 import pkgutil
+import importlib
 
 __all__ = []
 PATH = [os.path.dirname(__file__)]
-for loader, module_name, is_pkg in pkgutil.walk_packages(PATH):
+
+for module_info in pkgutil.walk_packages(PATH):
+    module_name = module_info.name  # Get module name correctly
     __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
+
+    # Import module dynamically using importlib
+    spec = importlib.util.find_spec(module_name, package=__name__)
+    if spec and spec.loader:
+        _module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(_module)
+        globals()[module_name] = _module
